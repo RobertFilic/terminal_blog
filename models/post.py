9f373d27623cf03604a6da0f35e74c9353102eaf
Robert <robertf@PC0351.local>
2018-04-13 17:43:46 +0300

    staticmethod --> classmethod

--- models/post.py
+++ models/post.py
@@ -35,11 +35,18 @@ class Post(object):
         }
 
     # Finding post content based on id
-    @staticmethod
-    def from_mongo(id):
-        return Database.find_one(collection='posts',
+    @classmethod
+    def from_mongo(cls, id):
+        post_data = Database.find_one(collection='posts',
                                  query={'id': id})  # Post.from_mongo('123') will give us the content with 'id' 123
+        return cls(blog_id=post_data['blog_id'],
+                   title=post_data['title'],
+                   content=post_data['content'],
+                   author=post_data['author'],
+                   date=post_data['create_date'],
+                   id=post_data['id'])
 
+    # Finding all blog posts in specific blog
     @staticmethod
     def from_blog(id):
         return Database.find(collection='posts', query={'blog_id': id})
